151a666d6f8bf42eafcb6289d417e1bb832ab143
Laio <laerciosoaresfragoso@gmail.com>
2024-05-28 16:09:12 -0300

    Aula 11 Condições

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 67.5
+
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+
+if (velocidade > 67) {
+    console.log('Você foi multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'USA'
+console.log('Vivendo em '+pais)
+if (pais == 'Brasil'){
+    console.log('Vc é Brasileiro!')
+}else{
+    console.log("Vc é um Estrangeiro!")
+}
