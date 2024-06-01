79d696150c8f4f73d7c6788707d6973f39f74a97
Laio <laerciosoaresfragoso@gmail.com>
2024-06-01 11:43:20 -0300

    Aula 14 Repetições (Parte 2)

--- Aula13/node_b625963a53cd8.tmp.js
+++
@@ -1,5 +0,0 @@
-var cont = 1
-
-while (cont <= 30){
-    console.log(`Passo ${cont}`)
-}
---
+++ Aula14/ambiente.js
@@ -0,0 +1,9 @@
+console.log("Vai começar...")
+for (var i = 1; i <= 5; i++ ){
+    console.log(`Teste ${i}`)
+}
+console.log("Fim!")
+
+//Para abrir a depuração F5.
+//Seleciona as variaveis e o inicio.
+//Para inicar F10.
