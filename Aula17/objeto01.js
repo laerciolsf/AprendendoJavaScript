1905f1c398f3ef4e700d4170149c3785d9de72b2
Laio <laerciosoaresfragoso@gmail.com>
2024-06-16 23:10:26 -0300

    Adicionando comentários

--- Aula17/objeto01.js
+++ Aula17/objeto01.js
@@ -1,8 +1,22 @@
-let amigo = {nome: 'José', sexo: 'M', peso: 70, 
-    engordar(p=0){
+// Declara um objeto chamado 'amigo' com propriedades e métodos
+let amigo = {
+    // Propriedade 'nome' com valor 'José'
+    nome: 'José',
+    // Propriedade 'sexo' com valor 'M'
+    sexo: 'M',
+    // Propriedade 'peso' com valor 70
+    peso: 70,
+    // Método 'engordar' que recebe um parâmetro 'p' com valor padrão 0
+    engordar(p=0) {
+        // Imprime 'Engordou!' no console
         console.log('Engordou!')
+        // Adiciona o valor de 'p' ao peso atual de 'amigo'
         this.peso += p
-    }}
+    }
+}
 
+// Chama o método 'engordar' do objeto 'amigo' passando 2 como argumento
 amigo.engordar(2)
+
+// Imprime o objeto 'amigo' atualizado no console
 console.log(amigo)
